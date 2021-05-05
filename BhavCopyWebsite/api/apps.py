453c192b5f346eb33279fc039b4a4781ca496d72
Aliabbas Merchant <merchantaliabbas@gmail.com>
2021-05-05 09:44:53 +0530

    Backend: Scheduled task to download and ingest data

--- BhavCopyWebsite/api/apps.py
+++ BhavCopyWebsite/api/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+    
+    def ready(self):
+        from apscheduler.schedulers.background import BackgroundScheduler
+        from .bhavcopy import download_and_ingest
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(download_and_ingest, 'cron', day_of_week='mon-fri', hour='18', minute='0')
+        scheduler.start()
