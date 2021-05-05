06c8a8c7782f0df89a19f10fd4c214b6b884187a
Aliabbas Merchant <merchantaliabbas@gmail.com>
2021-05-05 16:31:40 +0530

    Frontend: Removed router and unnecessary scrip page

--- BhavCopyWebsite/frontend/src/App.vue
+++ BhavCopyWebsite/frontend/src/App.vue
@@ -1,17 +1,43 @@
 <template>
-  <router-view id="nav" />
+  <div id="home">
+    <Search class="mb-3" @search="onSearchResult" />
+    <template v-if="searchResults != undefined">
+      <ResultsView :scrips="searchResults" />
+      <div v-if="searchResults.length === 0">No Data</div>
+    </template>
+  </div>
 </template>
 
+<script>
+import Search from "@/components/Search.vue";
+import ResultsView from "@/components/ResultsView.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      searchResults: undefined, // to indicate that no search has been performed as yet
+    };
+  },
+  components: {
+    Search,
+    ResultsView,
+  },
+  methods: {
+    onSearchResult(data) {
+      this.searchResults = data;
+    },
+  },
+};
+</script>
+
 <style>
-#nav {
+#home {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
   padding: 20px;
 }
 </style>
