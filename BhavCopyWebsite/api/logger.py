6805ebe328684ce1c7804eabacd75cb00492827f
Aliabbas Merchant <merchantaliabbas@gmail.com>
2021-05-05 16:51:37 +0530

    Backend: Added Logging to Files

--- BhavCopyWebsite/api/logger.py
+++ BhavCopyWebsite/api/logger.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 
 logger = logging.getLogger("bhavcopy")
 logger.propagate = False
@@ -6,8 +7,13 @@ logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s", "%b %d %H:%M:%S")
 
+file_handler = logging.FileHandler("log/" + datetime.strftime(datetime.now(), "%b %d %H:%M:%S") + ".log")
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
 stream_handler.setLevel(logging.DEBUG)
 
+logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
